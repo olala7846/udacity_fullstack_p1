1db20a9a82f1dad9b144ae26ceb43fc2ecf08bba
hsincheng Chao <olala7846@gmail.com>
2015-08-15 20:13:27 +0800

    Update Python comment and documents

--- MovieWebsite/entertaiment_center.py
+++ MovieWebsite/entertaiment_center.py
@@ -1,6 +1,7 @@
 import fresh_tomatoes
 import media
 
+# Instanitiate movie objects and put them into movie_list array
 avatar = media.Movie("Avatar", "A marine on a alian planet", 
         "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", 
         "https://youtu.be/cRdxXPV9GNQ")
@@ -13,7 +14,8 @@ mi5 = media.Movie("Mission: Impossible",
         "After intercepting nerve gas being sold to terrorists...",
         "https://upload.wikimedia.org/wikipedia/en/f/fb/Mission_Impossible_Rogue_Nation_poster.jpg",
         "https://youtu.be/E66Cimpp9Vw")
-
-
 movie_list = [avatar, inside_out, mi5]
+
+# Generate the static html with fresh_Tomatoes.py
 fresh_tomatoes.open_movies_page(movie_list)
+
